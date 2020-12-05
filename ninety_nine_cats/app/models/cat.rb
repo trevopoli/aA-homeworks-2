e4ec27be61edcda4cff3f3b5ce2f2a5452d1c8da
trevopoli <trevopoli@gmail.com>
2020-12-05 15:35:29 -0700

    create cats controller and index view

--- ninety_nine_cats/app/models/cat.rb
+++ ninety_nine_cats/app/models/cat.rb
@@ -1,9 +1,15 @@
 require 'action_view'
 
 class Cat < ApplicationRecord
-    ActionView::Helpers::DateHelper
+    include ActionView::Helpers::DateHelper
+
+    CAT_COLORS = ["black", "white", "orange", "brown"].freeze
+
+    validates :color, inclusion: CAT_COLORS
+    validates :sex, inclusion: ["M", "F"]
+    validates :birth_date, :name, :color, :sex, presence: true
 
     def age
-        time_ago_in_words(Time.new - self.birth_date)
+        time_ago_in_words(birth_date)
     end
 end
