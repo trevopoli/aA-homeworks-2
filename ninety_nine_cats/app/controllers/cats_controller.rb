ad26ed70e938ed73c07c1413f77a9201e609f165
trevopoli <trevopoli@gmail.com>
2020-12-06 10:31:48 -0700

    views for cat new/edit

--- ninety_nine_cats/app/controllers/cats_controller.rb
+++ ninety_nine_cats/app/controllers/cats_controller.rb
@@ -4,4 +4,44 @@ class CatsController < ApplicationController
         @cats = Cat.all
         render :index
     end
+
+    def show
+        @cat = Cat.find_by(id: params[:id])
+        render :show
+    end
+
+    def new
+        @cat = Cat.new
+        render :new
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @cat = Cat.find_by(id: params[:id])
+        render :edit
+    end
+
+    def update
+        @cat = Cat.find_by(id: params[:id])
+
+        if @cat.update_attributes(cat_params)
+            redirect_to cat_url(@cat)
+        else
+            render :edit
+        end
+    end
+
+    private
+    def cat_params
+        params.require(:cat).permit(:name, :birth_date, :color, :sex, :description)
+    end
 end
