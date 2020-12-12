1717f663a5f9ce93c403dcd7eef92fb9aa971c2b
trevopoli <trevopoli@gmail.com>
2020-12-11 21:19:55 -0700

    User and session helpers

--- ninety_nine_cats/app/controllers/sessions_controller.rb
+++ ninety_nine_cats/app/controllers/sessions_controller.rb
@@ -1,3 +1,27 @@
 class SessionsController < ApplicationController
-    
+
+    def new
+        render :new
+    end
+
+    def create
+        user = User.find_by_credentials(
+            params[:user][:user_name],
+            params[:user][:password]
+        )
+        
+        if user.nil?
+            flash.now[:errors] = ["Incorrect username and/or password"]
+            render :new
+        else
+            login_user!(user)
+            redirect_to cats_url
+        end
+    end
+
+    def destroy
+        logout_user!
+        redirect_to new_session_url
+    end
+
 end
--- ninety_nine_cats/app/controllers/users_controller.rb
+++ ninety_nine_cats/app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.new(user_params)
-        if user.save
-            # login
+        @user = User.new(user_params)
+        if @user.save
+            login_user!(@user)
             redirect_to cats_url
         else
             flash.now[:errors] = @users.errors.full_messages
