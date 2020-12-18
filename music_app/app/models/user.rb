7083249629d853d7e9086045d5cacfec12c9a0e5
trevopoli <trevopoli@gmail.com>
2020-12-17 19:40:53 -0700

    User model with credentials

---
+++ music_app/app/models/user.rb
@@ -0,0 +1,42 @@
+class User < ApplicationRecord
+    validates :password, length: {minimum: 6, allow_nil: true}
+    validates :email, presence: true, uniqueness: true
+    validates :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+
+    attr_reader :password
+
+    after_initialize :ensure_session_token
+
+    def self.generate_session_token
+        SecureRandom.urlsafe_base64(16)
+    end
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    private
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+
+end
