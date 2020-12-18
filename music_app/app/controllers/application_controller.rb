4b113bd519db33d5cc22e30a8357a4c543651a5e
trevopoli <trevopoli@gmail.com>
2020-12-17 20:02:17 -0700

    controllers for User and Session

--- music_app/app/controllers/application_controller.rb
+++ music_app/app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
+
+    def log_in_user!(user)
+        user.reset_session_token!
+        session[:session_token] = user.session_token
+    end
+
+    def log_out_user!
+        current_user.reset_session_token!
+        session[:session_token] = nil
+    end
+
 end
---
+++ music_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+    def new
+        render :new #for user to login
+    end
+
+    def create
+        user = User.find_by_credentials(
+            params[:user][:email],
+            params[:user][:password])
+
+        if user
+            log_in_user!(user)
+            redirect_to user_url(@user.id)
+        else
+            flash.now[:errors] = ["Incorrent email and/or password."]
+            render :new
+        end
+    end
+
+    def destroy
+        log_out_user
+    end
+end
