8b28859d4c230e71cbdff0794fe1e496a0b164ca
trevopoli <trevopoli@gmail.com>
2021-01-07 21:14:08 -0500

    added auth_spec

--- goalsetting_app/spec/features/auth_spec.rb
+++ goalsetting_app/spec/features/auth_spec.rb
@@ -22,13 +22,33 @@ end
 
 
 feature 'logging in' do
-  scenario 'shows username on the homepage after login'
+  before(:each) do
+    visit new_user_url
+    fill_in 'email', with: 'redman@gmail.com'
+    fill_in 'password', with: 'good_password'
+    click_on 'Create Account'
+    click_on 'Log Out'
+  end
 
+  scenario 'shows username on the homepage after login' do
+    visit new_session_url
+    fill_in 'email', with: 'redman@gmail.com'
+    fill_in 'password', with: 'good_password'
+    click_button 'Log In'
+    expect(page).to have_content 'redman@gmail.com'
+  end
 end
 
 feature 'logging out' do
-  scenario 'begins with a logged out state'
+  before(:each) do
+    visit new_user_url
+    fill_in 'email', with: 'blueman@gmail.com'
+    fill_in 'password', with: 'good_password'
+    click_on 'Create Account'
+  end
 
-  scenario 'doesn\'t show username on the homepage after logout'
-
+  scenario 'begins with a logged out state' do
+    click_on 'Log Out'
+    expect(page).to have_content 'Log In'
+  end
 end
