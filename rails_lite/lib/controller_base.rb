9f589e6e397a2d25890fcd94c942ad826f33fdfb
trevopoli <trevopoli@gmail.com>
2021-02-09 20:33:49 -0700

    completed session_server

--- rails_lite/lib/controller_base.rb
+++ rails_lite/lib/controller_base.rb
@@ -24,6 +24,7 @@ class ControllerBase
     @res.location = url
     @res.status = 302
     @already_built_response = true
+    session.store_session(@res)
   end
 
   # Populate the response with content.
@@ -34,15 +35,29 @@ class ControllerBase
     @res['Content-Type'] = content_type
     @res.write(content)
     @already_built_response = true
+    session.store_session(@res)
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    template_fname = File.join(
+      dir_path, "..",
+      "views", self.class.name.underscore, "#{template_name}.html.erb"
+    )
+
+    template_code = File.read(template_fname)
+
+    render_content(
+      ERB.new(template_code).result(binding),
+      "text/html"
+    )
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
