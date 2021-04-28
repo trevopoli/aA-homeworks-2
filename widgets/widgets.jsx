4c619b88aca00239e25f1dd2783638717a6495b8
trevopoli <trevopoli@gmail.com>
2021-04-27 18:29:11 -0700

    framework for new React project

---
+++ widgets/widgets.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const main = document.getElementById("main");
+
+    const Root = () => (
+        <div>
+        </div>
+    );
+    
+    ReactDOM.render(<Root />, main);
+});
