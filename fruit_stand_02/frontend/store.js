f9054b8e4ad6265d1e8b4cc5ce807f689e23b875
trevopoli <trevopoli@gmail.com>
2021-05-30 15:28:42 -0700

    added redux dev tools for fruit_stand

--- fruit_stand_02/frontend/store.js
+++ fruit_stand_02/frontend/store.js
@@ -1,6 +1,17 @@
-import { createStore } from 'redux';
 import reducer from './reducer.js';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-const Store = createStore(reducer);
+// const Store = createStore(reducer);
 
-export default Store;
+const store = (preloadedState = {}) => (
+    createStore(
+        reducer,
+        preloadedState,
+        composeWithDevTools(applyMiddleware(thunk, logger))
+    )
+);
+
+export default store;
