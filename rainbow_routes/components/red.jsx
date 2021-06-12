86f8f38528fce3a8faf31e41060a43097e205089
trevopoli <trevopoli@gmail.com>
2021-06-11 18:21:39 -0700

    added NavLinks and Routes for rainbow components

--- rainbow_routes/components/red.jsx
+++ rainbow_routes/components/red.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Link, NavLink } from 'react-router-dom';
+import rootReducer from '../../pokedex/frontend/reducers/root_reducer';
 import Orange from './orange';
 import Yellow from './yellow';
 
@@ -8,9 +9,12 @@ class Red extends React.Component {
     return(
       <div>
         <h2 className="red"></h2>
-        {/* Links here */}
+        <NavLink exact to="/red">Red Only</NavLink>
+        <NavLink to="/red/orange">Add Orange</NavLink>
+        <NavLink to="/red/yellow">Add Yellow</NavLink>
 
-        {/* Routes here */}
+        <Route path="/red/orange" component={Orange} />
+        <Route path="/red/yellow" component={Yellow} />
       </div>
     );
   }
